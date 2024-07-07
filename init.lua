6860f60c5302379e53d58734b94c6bea95e083b2
Jay Moker <dadamien78@gmail.com>
2024-07-07 19:30:47 +0200

    Basic treesitter config + some colorscheme

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ vim.call('plug#begin')
 
 Plug ('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 
+-- COLOR SCHEME 
+Plug ('nordtheme/vim')				-- Nord
+Plug ('gilgigilgil/anderson.vim')	-- Anderson 
+
 vim.call('plug#end')
 
 require "config.options"
+require "config.treesitter"
