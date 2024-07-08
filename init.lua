bc4103cea4f94d60ce9665b360d98ee1d926fd5d
Jay Moker <dadamien78@gmail.com>
2024-07-08 11:13:12 +0200

    Starting to add code completion

--- init.lua
+++ init.lua
@@ -11,15 +11,18 @@ Plug ('gilgigilgil/anderson.vim')	-- Anderson
 Plug ('sts10/vim-pink-moon')		-- Pink-moon
 
 -- CODE COMPLETION 
+Plug ('hrsh7th/nvim-cmp')			-- The completion Plugin
+Plug ('hrsh7th/cmp-buffer')			-- buffer completion
+Plug ('hrsh7th/cmp-path')			-- path completion
+Plug ('hrsh7th/cmp-cmdline')		-- cmd_line completion
+Plug ('saadparwaiz1/cmp_luasnip')	-- luasnip completion
+
 -- Plug ('neovim/nvim-lspconfig')
 -- Plug ('hrsh7th/cmp-nvim-lsp')
--- Plug ('hrsh7th/cmp-buffer')
--- Plug ('hrsh7th/cmp-path')
--- Plug ('hrsh7th/cmp-cmdline')
--- Plug ('hrsh7th/nvim-cmp')
 
--- SNIPPETS
--- Plug ('L3MON4D3/LuaSnip', {'tag': 'v2.\*', 'do': 'make install_jsregexp'}) -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+-- SNIPPET ENGINE
+Plug ('L3MON4D3/LuaSnip', {['tag'] = 'v2.*', ['do'] = 'make install_jsregexp'})
+Plug ('rafamadriz/friendly-snippets')	-- Add tons of snippets
 
 vim.call('plug#end')
 
@@ -30,3 +33,4 @@ require "config.keymaps"
 
 -- LOAD PLUGINS
 require "plugin.treesitter"
+require "plugin.cmp"
